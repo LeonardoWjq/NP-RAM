3c0060340699dd25a342c0ad96d2fd7b516211b8
Jiuqi Wang <leonardowang597@gmail.com>
2023-09-25 09:19:34 -0400

    create setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NP-RAM',
+    version='0.0.1',
+    description='Natural-Language Powered Robotic Arm Manipulation',
+    author='Jiuqi Wang, Yufeng Gao',
+    packages=find_packages()
+)
