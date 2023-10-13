f68968333f41d3060ef9dab22f74197310763978
Jiuqi Wang <leonardowang597@gmail.com>
2023-10-13 16:57:10 -0400

    update

--- demos/robosuite_demo.py
+++ demos/robosuite_demo.py
@@ -1,25 +1,57 @@
+import os
+
+import h5py
 import numpy as np
-import robosuite as suite
+
 import robomimic
+import robosuite as suite
+from utils.data import make_path, read_xml, process_xml
+import robomimic.utils.env_utils as EnvUtils
+import robomimic.utils.file_utils as FileUtils
+import robomimic.utils.obs_utils as ObsUtils
 
-# create environment instance
-env = suite.make(
-    env_name="PickPlaceBread", # try with other tasks like "Stack" and "Door"
-    robots="Sawyer",  # try with other robots like "Sawyer" and "Jaco"
-    has_renderer=False,
-    has_offscreen_renderer=False,
-    use_camera_obs=False,
-)
 
-# reset the environment
-env.reset()
+data_path = make_path('RoboTurkPilot', 'bins-full', 'low_dim.hdf5')
+# data_path = make_path('robomimic','datasets','can','ph','low_dim_v141.hdf5')
 
-for i in range(1):
-    action = np.random.randn(env.robots[0].dof) # sample random action
-    print(action.shape)
-    obs, reward, done, info = env.step(action)  # take action in the environment
-    for key, value in obs.items():
-        print(key, value.shape)
-    # env.render()  # render on display
+env_meta = FileUtils.get_env_metadata_from_dataset(data_path)
+env_meta['env_kwargs']['has_renderer'] = True
+env = suite.make(env_name=env_meta['env_name'],
+                 **env_meta['env_kwargs'])
+# env = EnvUtils.create_env_from_metadata(
+#     env_meta=env_meta,
+#     render=False, 
+#     render_offscreen=False,
+#     use_image_obs=False
+# )
+
+with h5py.File(data_path, 'r') as f:
+    data = f['data']
+    demo = data['demo_2']
+    states = demo['states']
+    actions = demo['actions']
+    xml_string = demo.attrs['model_file']
+    env.reset_from_xml_string(process_xml(xml_string))
+    env.sim.set_state_from_flattened(states[1])
+    for action in actions:
+        obs, reward, done, info = env.step(action)
+        env.render()
 
 env.close()
+
+
+
+# model_path = make_path('RoboTurkPilot', 'bins-Can', 'models', 'model_1.xml')
+# xml_string = read_xml(model_path)
+
+# env.reset_from_xml_string(xml_string)
+
+# with h5py.File(data_path, 'r') as f:
+#     data = f['data']
+#     demo = data['demo_1']
+
+    # for action in actions:
+    #     obs, reward, done, info = env.step(action)
+    #     env.render()
+
+# env.close()
--- utils/data.py
+++ utils/data.py
@@ -1,14 +1,31 @@
 import os
+import re
 
 
 def make_path(*args):
     return os.path.join(os.getcwd(), *args)
 
 
+def read_xml(xml_path: str):
+    with open(xml_path, 'r') as f:
+        xml_string = f.read()
+    return xml_string
+
+
+def process_xml(xml_string: str):
+    robosuite_path = make_path('robosuite')
+    return re.sub(r'/home/robot/installed_libraries/robosuite',
+                  robosuite_path,
+                  xml_string)
+
+
 if __name__ == '__main__':
-    path = make_path(
+    xml_path = make_path(
         'RoboTurkPilot',
         'bins-Can',
-        'low_dim.hdf5'
+        'models',
+        'model_1.xml'
     )
-    print(path)
+    xml_string = read_xml(xml_path)
+    xml_string = process_xml(xml_string)
+    print(xml_string)
