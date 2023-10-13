2d03d1d2175cc84ade06f2eecb5930453a2a3b07
Jiuqi Wang <leonardowang597@gmail.com>
2023-10-13 13:21:00 -0400

    update data utils

--- utils/data.py
+++ utils/data.py
@@ -1,16 +1,14 @@
 import os
 
-import h5py
 
-def make_path(path: str):
-    return os.path.join(os.getcwd(), path)
+def make_path(*args):
+    return os.path.join(os.getcwd(), *args)
+
 
 if __name__ == '__main__':
-    path = make_path(os.path.join('RoboTurkPilot', 'bins-Bread', 'demo.hdf5'))
-    with h5py.File(path) as f:
-        data = f['data']
-        # print(list(data))
-        group = data['demo_2']
-        for key in group.keys():
-            print(group[key])
-        # print(state[:10])
+    path = make_path(
+        'RoboTurkPilot',
+        'bins-Can',
+        'low_dim.hdf5'
+    )
+    print(path)
