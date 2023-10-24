94546607b2b6e5ef49d2f2da746992d5b6112624
Jiuqi Wang <leonardowang597@gmail.com>
2023-10-24 00:16:52 -0400

    implement stack dataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,21 +1,51 @@
-import os
-
 import h5py
-from torch.utils.data import Dataset
+import numpy as np
+from torch.utils.data import DataLoader, Dataset
 
 from utils.data_utils import make_path
 
 
-class GymDataset(Dataset):
-    def __init__(self, dataset_path: str) -> None:
+class StackDataset(Dataset):
+    def __init__(self, train: bool = True) -> None:
         super().__init__()
-        self.dataset_path = dataset_path
-        with h5py.File(dataset_path, 'r') as f:
-            self.data = f['data']['demo_0']
-            print(self.data)
+        if train:
+            dataset_path = make_path(
+                'datasets', 'train', 'trajectory_state.h5')
+        else:
+            dataset_path = make_path(
+                'datasets', 'validation', 'trajectory_state.h5')
+
+        self.instructions = []
+        self.obs = []
+        self.actions = []
+
+        with h5py.File(dataset_path, 'r') as data:
+            for traj in data.values():
+                obs = traj['obs'][:]
+                actions = traj['actions'][:]
+                ins = traj['instruction'][:]
+                ins = np.repeat(ins[None], len(obs), axis=0)
+                self.instructions.append(ins)
+                self.obs.append(obs)
+                self.actions.append(actions)
+
+        self.instructions = np.concatenate(self.instructions, axis=0)
+        self.obs = np.concatenate(self.obs, axis=0)
+        self.actions = np.concatenate(self.actions, axis=0)
+
+        assert len(self.instructions) == len(self.obs) == len(self.actions)
+
+    def __len__(self) -> int:
+        return len(self.obs)
+
+    def __getitem__(self, idx: int) -> tuple:
+        return self.instructions[idx], self.obs[idx], self.actions[idx]
 
 
 if __name__ == '__main__':
-    path = make_path(os.path.join('robomimic', 'datasets', 'can', 'ph', 'low_dim_v141.hdf5'))
-    dataset = GymDataset(path)
-
+    dataset = StackDataset()
+    dataloader = DataLoader(dataset, batch_size=32,
+                            shuffle=True, num_workers=4)
+    for ins, obs, actions in dataloader:
+        print(ins.shape, obs.shape, actions.shape)
+        break
