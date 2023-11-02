b7ff64690c8f144cf2eeebc07518dcedfe84e073
Jiuqi Wang <leonardowang597@gmail.com>
2023-11-01 22:55:17 -0400

    add helper function

--- utils/train_utils.py
+++ utils/train_utils.py
@@ -1,8 +1,8 @@
 import pathlib
 
 from utils.data_utils import make_path
-
-
+import numpy as np
+from collections import deque
 def make_log_dir(log_dir: str, model: str):
     checkpoint_path = make_path(log_dir, model, 'checkpoints')
     tensorboard_path = make_path(log_dir, model, 'tensorboard')
@@ -10,3 +10,12 @@ def make_log_dir(log_dir: str, model: str):
     pathlib.Path(checkpoint_path).mkdir(parents=True, exist_ok=True)
     pathlib.Path(tensorboard_path).mkdir(parents=True, exist_ok=True)
     pathlib.Path(video_path).mkdir(parents=True, exist_ok=True)
+
+def init_deque(obs: np.ndarray, seq_len: int):
+    sequence = np.repeat(obs[None], seq_len, axis=0)
+    return deque(sequence, maxlen=seq_len)
+
+def update_deque(obs: np.ndarray, window: deque):
+    window.append(obs)
+    return np.array(window)
+
